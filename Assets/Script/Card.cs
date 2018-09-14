c1a69087e97dff3d905e2e7042686e0886300cc1
Emiliano Pineda <emi.rock11@hotmail.com>
2018-09-14 18:42:22 -0500

    Se Añadieron los sprites de conclusión del juego

---
+++ Assets/Script/Card.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject MainG;
+    private bool used=false;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Restart() {
+        used = false;
+    }
+
+    void OnMouseDown()
+    {
+        
+        if (!used)
+        {
+            MainG.SendMessage("OnCardClick", this.gameObject.GetComponent<Renderer>().material);
+            used = true;
+        }
+    }
+}
--- Assets/Script/Game.cs
+++ Assets/Script/Game.cs
@@ -1,36 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
 
 public class Game : MonoBehaviour {
-
+    public GameObject Button;
     public Material[] Materials;
     public GameObject[] Cartas;
     public Material[] Numbers;
     public GameObject CardNumber;
     public GameObject SelectionCard;
-    
+    private int contObjective = 0;
+    private int actualCont = 0;
+    private Material materialObj = null;
+    public GameObject WinSpryte;
+    public GameObject LooseSpryte;
+
 
 
     // Use this for initialization
     void Start () {
-        int cardNumIndex = Random.Range(0,3);
+
+        StartGame();
+    }
+
+    void OnCardClick(Material cardMaterial)
+    {
+        string materialName = cardMaterial.mainTexture.name;
+        string materialObjString = materialObj.mainTexture.name;
+
+        if(materialName == materialObjString)
+        {
+            actualCont++;
+            if(actualCont == contObjective)
+            {
+                //Ya ganaste
+                Debug.Log("Ganaste");
+                Button.SetActive(true);
+                WinSpryte.SetActive(true);
+            }
+        }
+        else
+        {
+            //Ya Perdiste
+            Debug.Log("Perdiste");
+            Button.SetActive(true);
+            LooseSpryte.SetActive(true);
+        }
+        
+    }
+
+    void Restart()
+    {
+        Button.SetActive(false);
+        StartGame();
+        for(int i=0; i < Cartas.Length; i++)
+        {
+            Cartas[i].SendMessage("Restart");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void StartGame()
+    {
+        WinSpryte.SetActive(false);
+        LooseSpryte.SetActive(false);
+        contObjective = 0;
+        actualCont = 0;
+        materialObj = null;
+        Button.SetActive(false);
+        int cardNumIndex = Random.Range(0, 3);
+        contObjective = cardNumIndex + 1;
         int selectionCardIndex = Random.Range(0, 3);
         SelectionCard.GetComponent<Renderer>().material = Materials[selectionCardIndex];
         CardNumber.GetComponent<Renderer>().material = Numbers[cardNumIndex];
-        SelectionCard.GetComponent<Renderer>().material = Materials[selectionCardIndex];
-        int contPerro=0;
-        int contChango=0;
-        int contVaca=0;
+        materialObj = Materials[selectionCardIndex];
+        int contPerro = 0;
+        int contChango = 0;
+        int contVaca = 0;
         bool isValidNum = false;
         int materialIndex = 0;
-        for (int i =0; i < Cartas.Length; i++) {
+        for (int i = 0; i < Cartas.Length; i++)
+        {
             while (!isValidNum)
             {
 
-                materialIndex = Random.Range(0,3);
+                materialIndex = Random.Range(0, 3);
 
                 switch (materialIndex)
                 {
@@ -40,7 +102,7 @@ public class Game : MonoBehaviour {
                             contPerro++;
                             isValidNum = true;
                         }
-                    break;
+                        break;
 
                     case 1:
                         if (contChango < 3)
@@ -58,16 +120,12 @@ public class Game : MonoBehaviour {
                         }
                         break;
                 }
-                
+
             }
             isValidNum = false;
             Cartas[i].GetComponent<Renderer>().material = Materials[materialIndex];
         }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
