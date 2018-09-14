ff978d206a39cf209a376e31f2b18b3d410ac56f
Emiliano Pineda <emi.rock11@hotmail.com>
2018-09-14 17:29:59 -0500

    Se acabó el setup y se ha randomizado las cartas

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Game : MonoBehaviour {
+
+    public Material[] Materials;
+    public GameObject[] Cartas;
+    public Material[] Numbers;
+    public GameObject CardNumber;
+    public GameObject SelectionCard;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        int cardNumIndex = Random.Range(0,3);
+        int selectionCardIndex = Random.Range(0, 3);
+        SelectionCard.GetComponent<Renderer>().material = Materials[selectionCardIndex];
+        CardNumber.GetComponent<Renderer>().material = Numbers[cardNumIndex];
+
+        for(int i =0; i < Cartas.Length; i++) {
+            int materialIndex = Random.Range(0, 3);
+            Cartas[i].GetComponent<Renderer>().material = Materials[materialIndex];
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
